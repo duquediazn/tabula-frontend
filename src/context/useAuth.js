6d3f3a758164bd44a7c915e0ce96ae6345dcb006
duquediazn <100313989+duquediazn@users.noreply.github.com>
2025-05-04 19:32:56 +0100

    Update useAuth.js

--- src/context/useAuth.js
+++ src/context/useAuth.js
@@ -3,13 +3,8 @@ import { useContext } from "react"; //Importa el hook useContext de React.
 import { AuthContext } from "./AuthProvider"; //Importa el contexto AuthContext, creado en AuthProvider.jsx
 
 export function useAuth() {
+  /*Esta función devuelve el valor actual del contexto AuthContext.
+   Evita repetir useContext(AuthContext) en todos los componentes.
+  */
   return useContext(AuthContext);
 }
-/*
-Esta función devuelve el valor actual del contexto AuthContext.
-
-¿Por qué es útil hacer esto así?
-- Evita repetir useContext(AuthContext) en todos los componentes.
-- Permite centralizar lógica adicional (por ejemplo, transformar datos antes de devolverlos).
-- Hace que el código sea más limpio y fácil de mantener.
-*/
